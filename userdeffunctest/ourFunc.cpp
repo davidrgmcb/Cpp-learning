f0de289bbf49c4f38c0659f909a59076e855cdc5
David <davidrgmcburney@gmail.com>
2017-04-19 11:07:25 -0700

    First of the day, function test, 4-19-17

---
+++ userdeffunctest/ourFunc.cpp
@@ -0,0 +1,21 @@
+// ourFunc.cpp -- Testing user defined function
+#include <iostream>
+void simon(int);
+
+int main()
+{
+    using namespace std;
+    simon(3);
+    cout << "Pick an integer: ";
+    int count;
+    cin >> count;
+    simon(count);
+    cout << "Done!" << endl;
+    return 0; 
+}
+
+void simon(int n)
+{
+    using namespace std;
+    cout << "Simon says touch your toes " << n << " times" << endl;
+}
